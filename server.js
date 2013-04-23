9534aad228f05c3b0b477f19fd9660071e992985
kmillard <kmillard@byu.edu>
2013-04-23 16:08:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-var io = require('socket.io').listen(process.env.port);
- 
-io.sockets.on('connection', function (socket) {
- 
-socket.on('message', function (message) {
-console.log("Got message: " + message);
-io.sockets.emit('pageview', { 'url': message });
-});
- 
-});
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
+
