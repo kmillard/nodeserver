34283a56209494c677f893cf17b8d0f808d0d6b6
kmillard <kmillard@byu.edu>
2013-04-23 15:21:15 -0600

    Changed to socket.io fuctionality

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Azure World\n');
-}).listen(port);
-
-
+var io = require('socket.io').listen(80);
+ 
+io.sockets.on('connection', function (socket) {
+ 
+socket.on('message', function (message) {
+console.log("Got message: " + message);
+io.sockets.emit('pageview', { 'url': message });
+});
+ 
+});
